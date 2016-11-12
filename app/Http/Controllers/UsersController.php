326376f792e8052227124934e11f905cc00f1d3a
Carolin <caro.guempel@web.de>
2016-11-12 16:07:32 +0100

    create / edit user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,11 +22,10 @@ class UsersController extends Controller
 
     public function edit($id)
     {
-        $user = new User();
-        if ($id !== '0') {
-            $user = User::findOrFail($id);
+        $user = ($id !== '0') ? User::findOrFail($id) : new User();
+        if (isset(session('_old_input')['name'])) {
+            $user->name = session('_old_input')['name'];
         }
-        $user->name = session('_old_input')['name'];
         $userRole = session('_old_input')['role'] ? session('_old_input')['role'] : $user->getRole();
         $roles = Role::all()->pluck('name');
         return view('pages.users.edit', ['user' => $user, 'userRole' => $userRole, 'roles' => $roles]);
@@ -34,22 +33,36 @@ class UsersController extends Controller
 
     public function save(Request $request, $id)
     {
-        // todo: save role and show error if user name already exist
-
         $request->flashOnly(['name', 'role']);
-
         $this->validate($request, [
             'name' => 'required|max:255',
             'password' => 'required|confirmed',
             'role' => 'required'
         ]);
 
-        $user = new User();
-        if ($id !== '0') {
-            $user = User::findOrFail($id);
+        // check that user name not exist
+        $user = User::where('name', '=', Input::get('name'))
+            ->where('id', '!=', $id)
+            ->first();
+        if ($user != null) {
+            return redirect()->to('users/'.$id.'/edit')
+                ->withErrors(['Username already exists.']);
         }
-        $user->fill(Input::all());
+
+        // find the role for the user
+        $role = Role::where('name', '=', Input::get('role'))->first();
+        if ($role == null) {
+            return redirect()->to('users/'.$id.'/edit')
+                ->withErrors(['Invalid role.']);
+        }
+
+        // save user
+        $user = ($id !== '0') ? User::findOrFail($id) : new User();
+        $user->name = Input::get('name');
+        $user->password = bcrypt(Input::get('password'));
         $user->saveOrFail();
+        $user->detachRoles();
+        $user->attachRole($role);
 
         return view('pages.users.index');
     }
